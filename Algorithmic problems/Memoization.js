6cab6e716d354d11eaac1c09f47e43b739610750
Vitor dos Santos Barcelos <progvitor@gmail.com>
2021-04-21 08:31:02 -0300

    solving problems

---
+++ Algorithmic problems/Memoization.js
@@ -0,0 +1,10 @@
+let f = [0, 1];
+
+const fibo = (n, memo={}) => {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo);
+    return memo[n];
+};
+
+console.log(fibo(1000))
