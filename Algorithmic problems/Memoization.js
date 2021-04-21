2d3b11cf8a8a4ca4971646fc11391c7c2f20155f
Vitor dos Santos Barcelos <progvitor@gmail.com>
2021-04-21 14:36:37 -0300

    memo

--- Algorithmic problems/Memoization.js
+++ Algorithmic problems/Memoization.js
@@ -8,3 +8,15 @@ const fibo = (n, memo={}) => {
 };
 
 console.log(fibo(1000))
+
+const gridTraveler = (m, n, memo={}) => {
+    const key = m + ',' + n;
+    if (key in memo) return memo[key]
+    if (m === 1 && n === 1) return 1;
+    if (m === 0 || n === 0) return 0;
+
+    memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+    return memo[key]
+};
+
+console.log(gridTraveler(100, 100))
