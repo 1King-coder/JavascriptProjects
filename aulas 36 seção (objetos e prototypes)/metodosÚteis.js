aa888255c54f52870963618282d42674be576b0e
Vitor dos Santos Barcelos <progvitor@gmail.com>
2021-04-30 16:20:33 -0300

    objects

--- aulas 36 seção (objetos e prototypes)/metodosÚteis.js
+++ aulas 36 seção (objetos e prototypes)/metodosÚteis.js
@@ -1,5 +1,6 @@
-const product = { nome: 'Canecas', preco: 1.8};
 
+const product = { nome: 'Canecas', preco: 1.8};
+/*
 const canecaPorcelana = { 
     ...product,
     nome:'Caneca de porcelana'
@@ -9,13 +10,34 @@ const canecaBarro = {
     ...product,
     nome: 'Caneca de Barro'
 };
+*/
 
-canecaBarro.material = 'Barro'
-canecaPorcelana.material = 'Porcelana'
+const canecaPorcelana = Object.assign({}, product,
+    {
+        nome: 'Caneca de porcelana',
+        material: 'Porcelana' 
+    });
+    
+    const canecaBarro = Object.assign({}, product,
+        { 
+            nome: 'Caneca de barro',
+            material: 'Barro' 
+        });
+        
+// canecaBarro.material = 'Barro';
+// canecaPorcelana.material = 'Porcelana';
 
-canecaBarro.preco = 19.99
-canecaPorcelana.preco = 25.00
+canecaBarro.preco = 19.99;
+canecaPorcelana.preco = 25.00;
 
-console.log(product)
-console.log(canecaPorcelana)
-console.log(canecaBarro)
+console.log(product);
+console.log(canecaPorcelana);
+console.log(canecaBarro);
+
+Object.defineProperty(product, 'nome', {
+    writable: false,
+    configurable: false
+});
+console.log(Object.getOwnPropertyDescriptor(product, 'nome'));
+
+console.log(Object.entries(canecaPorcelana))
