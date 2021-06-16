bbfc7c96eda45fb123a1f37c7897fceabab676cc
Vitor dos Santos Barcelos <progvitor@gmail.com>
2021-06-16 08:58:24 -0300

    Learning express

--- Node/aula-6-(Express)/Static-files/server.js
+++ Node/aula-6-(Express)/Static-files/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
