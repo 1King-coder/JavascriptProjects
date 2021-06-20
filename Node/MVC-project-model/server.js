cd26094e05d0184e14dea214f7074bbddfc0b47f
Vitor dos Santos Barcelos <progvitor@gmail.com>
2021-06-20 18:21:46 -0300

    MVC project model

--- Node/MVC-project-model/server.js
+++ Node/MVC-project-model/server.js
@@ -22,7 +22,9 @@ const helmet = require('helmet');
 const csrf = require('csurf');
 const { globalMiddleware, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware')
 
-// app.use(helmet());   Only use when with HTTPS
+app.use(helmet());
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));  // Allow request.body
 app.use(express.static(path.resolve(__dirname, 'public')));
 
