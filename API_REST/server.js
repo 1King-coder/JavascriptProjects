52e254970bced76b4124b760f483298fb5893778
Vitor <progvitor@gmail.com>
2021-07-15 18:23:24 -0300

    moved API_REST

--- API_REST/server.js
+++
@@ -1,7 +0,0 @@
-import app from './app';
-
-const port = 3001;
-
-app.listen(port, () => {
-  console.log(`Server online. \nhttp://localhost:${port}`);
-});
--- API_REST/src/routes/tokenR.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import tokenController from '../controllers/TokenC';
-
-const router = new Router();
-
-router.post('/', tokenController.store);
-
-export default router;
