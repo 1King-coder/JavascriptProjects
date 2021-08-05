1fc5354dd1d93296ec54f267c56719bbf09a6224
1King-coder <progvitor@gmail.com>
2021-08-05 12:54:50 -0300

    Creating Navbar and installed react-icons

--- ReactJs/react-base/.eslintrc.js
+++ ReactJs/react-base/.eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
     'import/prefer-default-export': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'jsx-a11y/anchor-is-valid': 0,
   },
 };
