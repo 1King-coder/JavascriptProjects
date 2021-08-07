f8cfb69c6c827f7236a0eb20e84c2f546f20c0f4
1King-coder <progvitor@gmail.com>
2021-08-07 20:39:52 -0300

    Changed BrowserRouter to MyRoute

--- ReactJs/react-base/src/App.js
+++ ReactJs/react-base/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
+import history from './services/history';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Header />
       <Routes />
       <GlobalStyle />
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- ReactJs/react-base/src/routes/index.js
+++ ReactJs/react-base/src/routes/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import MyRoute from './myRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/404';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="*" component={Page404} />
+      <MyRoute exact path="/" component={Login} isClosed />
+      <MyRoute path="*" component={Page404} />
     </Switch>
   );
 }
